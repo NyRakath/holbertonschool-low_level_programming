0dc7da407d72568766dbb5ad6458ae08c75936c7
NyRakath <soncor23@gmail.com>
2021-10-01 14:03:18 -0500

    3Lowercase&Uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - mmm
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 97; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
+
