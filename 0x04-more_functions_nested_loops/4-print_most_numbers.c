c6426e8ef4ce368c5016fe6a9703c6a2a52615bf
NyRakath <soncor23@gmail.com>
2021-10-08 13:36:30 -0500

    4 print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - 0 at 9 numbers
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
 int mul(int a, int b);
+void print_most_numbers(void);
 #endif /*MAIN_H*/
