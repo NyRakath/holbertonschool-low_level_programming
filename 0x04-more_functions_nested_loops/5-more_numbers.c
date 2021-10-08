347b3ec3f76ac784d77c5d4aed2f5f861d229fc4
NyRakath <soncor23@gmail.com>
2021-10-08 14:15:34 -0500

    5 more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - numbers 1 to 14
+ */
+
+void more_numbers(void)
+{
+	int forin, forex;
+
+	for (forex = 0; forex < 10; forex++)
+	{
+		for (forin = 0; forin <= 14; forin++)
+		{
+			if (forin > 9)
+				{
+				_putchar(forin / 10 + '0');
+				}
+		_putchar(forin % 10 + '0');
+		}
+	putchar(10);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,4 +10,5 @@ int _isdigit(int c);
 void print_numbers(void);
 int mul(int a, int b);
 void print_most_numbers(void);
+void more_numbers(void);
 #endif /*MAIN_H*/
