79ae81c20e4aa1f98c4dcb0fa7ff686dc7a5440f
NyRakath <soncor23@gmail.com>
2021-10-08 11:58:55 -0500

    0 isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*Define libraries*/
+#include <unistd.h>
+#include <stdio.h>
+int _isupper(int c);
+#endif /*MAIN_H*/
