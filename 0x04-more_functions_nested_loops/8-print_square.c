274a66751ae19d19f5dcd3574da9ad6c734d48ca
NyRakath <soncor23@gmail.com>
2021-10-08 17:19:57 -0500

    8 print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - Print a sqare an equals dimensions height and weight
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int y, x;
+
+	if (size <= 0)
+		for (y = 0; y < size; y++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
+		}
+}
