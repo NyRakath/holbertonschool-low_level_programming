985afd3bd5fe97ec225ddff0e737db5e7c279745
NyRakath <soncor23@gmail.com>
2021-10-08 13:08:20 -0500

    2 Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiplier two values
+ * @a: first value
+ * @b: two value
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,6 @@
 #include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
+void print_numbers(void);
+int mul(int a, int b);
 #endif /*MAIN_H*/
