9c8020d0e3cf60d976eea8605199c58740e28978
NyRakath <soncor23@gmail.com>
2021-10-08 16:52:47 -0500

    6 Print Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - Print a line in terminal
+ *
+ * @n: Input number of lines to print
+ */
+
+void print_line(int n)
+{
+	int i; 
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');	
+	}
+ 	_putchar(10);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,4 +11,6 @@ void print_numbers(void);
 int mul(int a, int b);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_diagonal(int n);
+void print_line(int n);
 #endif /*MAIN_H*/
