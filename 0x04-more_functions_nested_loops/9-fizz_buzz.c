d644d293c2d2748ed92e995eab738271f696899d
NyRakath <soncor23@gmail.com>
2021-10-08 18:21:10 -0500

    9 Fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
