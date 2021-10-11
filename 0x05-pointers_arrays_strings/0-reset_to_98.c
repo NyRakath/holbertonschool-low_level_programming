2dbf6ea48357952b627928d48e6adf1790989c90
NyRakath <soncor23@gmail.com>
2021-10-11 11:25:03 -0500

    0-reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Change value of n at 98
+ * @a: pointer of a or n
+ */
+
+void reset_to_98(int *a)
+{
+	*a = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*Define libraries*/
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+void reset_to_98(int *n);
+#endif /*MAIN_H*/
