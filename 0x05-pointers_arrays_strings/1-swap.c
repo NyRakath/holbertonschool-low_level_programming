c758852671adb0d66b4139608396275468c6d9b7
NyRakath <soncor23@gmail.com>
2021-10-11 11:48:02 -0500

    1 swap changes value

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Change value of *p1 and *p2
+ * @p1: Pointer 1
+ * @p2: Pointer 2
+ */
 
+void swap_int(int *p1, int *p2)
+{
+	int a = *p1;
+
+	*p1 = *p2;
+	*p2 = a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@
 #include <stdio.h>
 #include <unistd.h>
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif /*MAIN_H*/
