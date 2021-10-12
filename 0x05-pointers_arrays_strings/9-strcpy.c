62f0edb24a146e3468c4fbfb0f4f0f04bc0d35ab
NyRakath <soncor23@gmail.com>
2021-10-11 23:34:46 -0500

    9 Duplicate a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - Copy a string and duplicate
+ * @dest: output 
+ * @src: Input
+ * Return:0
+ */
+char *_strcpy(char *dest, char *scr)
+{
+	int ardd = 0;
+
+	for (ardd = 0; src[ardd] != '\0'; ardd++)
+	{
+		dest[ardd] = src[ardd];
+	} 
+	dest[ardd] = src[ardd];
+	return (dest);
+}
