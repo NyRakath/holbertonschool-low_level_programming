ebe8dfef2a122b8346e4e2f883f1180b4b218ee1
NyRakath <soncor23@gmail.com>
2021-10-14 14:07:16 -0500

    [FIX] 3-strcmp Betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,23 +8,23 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
-    int comp = 0;
-    
-    while(s1[i] != '\0' && s2[i] != '\0')
-    {
-        if (s1[i] == s2[i])
-        {
-            comp = 0;
-            i++;
-            
-        }
-        else
-        {
-            comp = s1[i] - s2[i];
-            break;
-        }
-    }
-    return (comp);
-    
+	int i = 0;
+	int comp = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			comp = 0;
+			i++;
+
+		}
+		else
+		{
+			comp = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (comp);
+
 }
