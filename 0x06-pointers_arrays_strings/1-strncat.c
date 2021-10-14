9192c9c7037bd488556c408a55e113c7722bdc37
NyRakath <soncor23@gmail.com>
2021-10-14 11:17:08 -0500

    [FIX] 1-strncat 0.1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -24,7 +24,6 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = _strlen(dest);
 	int i = 0;
-	char valUpper[]; {','';''.''!''=''"''('')''{''}'' ''\n'}
 
 	for (; ((i < n) && (src[i] != '\0')); i++)
 		dest[dest_len + i] = src[i];
