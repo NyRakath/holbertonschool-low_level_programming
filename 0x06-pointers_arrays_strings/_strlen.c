3340d8cdf8cf25155933316b84c3d4cd82c3be4f
NyRakath <soncor23@gmail.com>
2021-10-14 11:16:04 -0500

    [ADD] 1-strncat

---
+++ 0x06-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Count the bytes of the lengh string
+ * @string: string
+ * Return: Value of the counter
+ */
+
+int _strlen(char *string)
+	{
+	  int counter=0;
+	 
+	  for (; string[counter] != '\0'; counter++)
+	    {
+	      }
+	      return(counter);
+	  }
+	
