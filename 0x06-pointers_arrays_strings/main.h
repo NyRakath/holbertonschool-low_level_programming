df939346a9d35a96922784487cec96bf6b5dba33
NyRakath <soncor23@gmail.com>
2021-10-14 13:59:22 -0500

    [ADD] 2-strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@ int _putchar(char c);
 int _strlen(char *string);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif /*MAIN_H*/
