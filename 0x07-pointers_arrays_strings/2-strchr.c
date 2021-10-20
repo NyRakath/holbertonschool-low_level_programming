7d3e5dba22c94e52e42c9fd4b5f2b5a2f448dea7
NyRakath <soncor23@gmail.com>
2021-10-19 23:03:00 -0500

    Second exersice

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _strchr - first ocurrence in a char
+* @s: char
+* @c: char
+* Return: string + itera
+*/
+char *_strchr(char *s, char c)
+{
+	int itera;
+
+	for (itera = 0; *(s + itera) != '\0'; itera++)
+		if (*(s + itera) == c)
+			return (s + itera);
+	if (*(s + itera) == c)
+		return (s + itera);
+	return (NULL);
+}
