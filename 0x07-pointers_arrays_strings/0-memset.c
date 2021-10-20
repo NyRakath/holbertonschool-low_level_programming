216f31969b026dfc4141493f4f6fe0484dbe6ae1
NyRakath <soncor23@gmail.com>
2021-10-19 22:41:37 -0500

    Fix exercise 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	char *pointer1 = s;
 
-	for (;n > 0 ; n--)
+	for (; n > 0 ; n--)
 	{
-		*p = b;
+		*pointer1 = b;
 		pointer1++;
-		
+
 	}
 	return (s);
 }
