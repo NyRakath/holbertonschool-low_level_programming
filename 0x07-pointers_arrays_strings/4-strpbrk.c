bff2b4313030e6c59c51c5e87e7d0d1240bd4ee9
NyRakath <soncor23@gmail.com>
2021-10-19 23:27:39 -0500

    4  Exercise a byte char compare a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strpbrk - search a byte char and print after
+ * @s: string 
+ * @accept: first char
+ * Return:s or NULL when 0 coincidence
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *buf = accept;
+
+	while (*s != '\0')
+	{
+		for (accept = buf; *accept != '\0'; accept++)
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
