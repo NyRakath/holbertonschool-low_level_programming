6d049dda14364874417e83271c03f93afe37be80
NyRakath <soncor23@gmail.com>
2021-10-21 22:31:23 -0500

    A number factorial with recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* factorial - Number of multiplier a number 
+* @n: Number factorial
+* Return: -1 error in recursion 0 good function or factorial funtion
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	else
+	return (n * factorial(n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,5 @@ void _puts_recursion(char *s);
 int is_prime_number(int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 #endif /*MAIN_H*/
