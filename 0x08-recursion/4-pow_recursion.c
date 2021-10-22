88925dbe3158ecd97bac59d3d646671967693444
NyRakath <soncor23@gmail.com>
2021-10-21 22:44:11 -0500

    FIx betty pow a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - potencia of a number 
+ * _pow_recursion - potencia of a number
  * @x: number base
  * @y: number pow
  * Return: -1 error recursion 1 end of pow or recursion of a pow
