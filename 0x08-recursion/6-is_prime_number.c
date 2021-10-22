58ed843187fea72eb1574e394ed7b334ade11f04
NyRakath <soncor23@gmail.com>
2021-10-21 23:48:45 -0500

    6 is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,32 +2,33 @@
 /**
  * is_prime_number - not is a number prime
  * @n: input
- * Return: value True is_prime 
+ * Return: value True is_prime
  */
 int is_prime_number(int n)
 {
-    if(n < 2)
-    {
-         return (0);
-    }
-    return (is_prime(n, 2));
+	if (n < 2)
+	{
+		return (0);
+	}
+	return (prime_number(n, 2));
 }
 /**
- * is_prime - is a number prime
+ * prime_number - is a number prime
  * @n: input
  * @i: 2
  * Return: conditions for a number prime
  */
-int is_prime(int n, int i)
+int prime_number(int n, int i)
 {
-  if(n != 2 && n % i == 0)
-  {
-      return (0);
-  }
-  
-  if(i > n / 2)
-  {
-      return (1);    
-  }
-return(is_prime(n, i + 1));
+	if (n != 2 && n % i == 0)
+	{
+		return (0);
+	}
+
+	if (i > n / 2)
+	{
+		return (1);
+	}
+	return (prime_number(n, i + 1));
+
 }
