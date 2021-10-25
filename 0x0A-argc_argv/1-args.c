2ec7229aa7cc7da5f0edeb82a0090a22c0a4402c
NyRakath <soncor23@gmail.com>
2021-10-25 13:45:07 -0500

    1 Args -  number of arguments print

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+	return (0);
+}
