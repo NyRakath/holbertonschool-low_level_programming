18aec24a5fdad021bf7c7c25dddf6086b6721d0d
NyRakath <soncor23@gmail.com>
2021-10-25 18:30:46 -0500

    1 Steps for create static lib

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * main - prints multiplies of argv[1] * argv[2]
+ * @argc: Program's argument count.
+ * @argv: Program's argument vector.
+ * Return: Always 0 (success).
+ */
