d4dffef33deaa056fda3e2cd274e083476cbf870
NyRakath <soncor23@gmail.com>
2021-10-25 13:25:01 -0500

    0-whatsmyname - print a name of a program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints program name
+ * @argc: Program's argument count.
+ * @argv: Program's argument vector. 
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#endif /*MAIN_H*/
