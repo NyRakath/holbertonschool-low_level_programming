3f0a2f5774af83e7b8fdfd17d8cf1e3f7cafade9
NyRakath <soncor23@gmail.com>
2021-10-27 23:38:17 -0500

    3 return a pointer in a array bidimensional

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Null if error exists or Grid allocated on succesfully execution.
+ */
+
+int **alloc_grid(int width, int height)
+{
+    int **gridAlloc;
+    int i = 0;
+    int j = 0;
+
+    if (width <= 0 || height <= 0)
+    {
+        return (NULL);
+    }
+
+    gridAlloc = (int **)malloc(height * sizeof(int *));
+
+    if (gridAlloc == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < height; i++)
+    {
+        gridAlloc[i] = (int *)malloc(width * sizeof(int));
+
+        if (gridAlloc[i] == NULL)
+        {
+            for (j = 0; j < height; j++)
+            {
+                free(gridAlloc[j]);
+            }
+            free(gridAlloc);
+            return (NULL);
+        }
+    }
+
+    return (gridAlloc);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,5 @@ char *create_array(unsigned int size, char c);
 void free_grid(int **grid, int height);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif /*MAIN_H*/
