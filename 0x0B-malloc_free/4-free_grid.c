60ce73306cc8617f0670d157b2e16f9040fd6645
NyRakath <soncor23@gmail.com>
2021-10-27 20:06:44 -0500

    4 - Use by free for liberate memory in the functi

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * *create_array - create a array with malloc and a character
- * @size: Size of array
- * @c: character asigment
- * Return: array chr or NULL for condictions
+ * free_grid - free variable memory in the heap for integer
+ * @grid: integers double pointer
+ * @height: free grid bidimensional
+ * Return:0
  */
 void free_grid(int **grid, int height)
 {
-    int iterator = 0;
+	int iterator = 0;
 
-    for (iterator = 0; iterator < height; iterator++)
-    {
-        free(grid[iterator]);
-    }
-    free(grid);
+	for (iterator = 0; iterator < height; iterator++)
+	{
+		free(grid[iterator]);
+	}
+	free(grid);
 
 }
