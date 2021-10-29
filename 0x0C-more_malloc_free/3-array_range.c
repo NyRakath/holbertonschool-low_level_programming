114c6454fe71bc41a089abeade6477eca311a75d
NyRakath <soncor23@gmail.com>
2021-10-28 21:47:31 -0500

    3 - array from min value until max value

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* *array_range - range of the a array from min - max
+* @min: min value for array
+* @max: max value for array
+* Return: pointer return of malloc or null error
+*/
+int *array_range(int min, int max)
+{
+	int *poin;
+	int total, i;
+
+	if(min > max)
+		return (NULL);
+	total = max - min + 1;
+	poin = malloc(sizeof(int) * total);
+	if (poin == NULL)
+		return (NULL);
+	for (i = 0; min <= max ; i++)
+	{
+		*(poin + i) = min;
+		min++;
+	}
+	return (poin);
+}
