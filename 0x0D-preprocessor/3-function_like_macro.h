987ad0342e3e576a7b66c94e77ad8d90321ef197
NyRakath <soncor23@gmail.com>
2021-11-01 13:12:51 -0500

    3 - protect parantheses

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,4 @@
 #ifndef HAVE_ABS
 #define HAVE_ABS
-#define ABS(x) (((x) < 0) ? (-(x)) : (x)) 
+#define ABS(x) ((x) < 0 ? (-(x)) : (x)) /* (-(x)) == ((x) * (-1)) law of signs... math logic */
 #endif
-
