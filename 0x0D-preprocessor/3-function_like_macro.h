725198d75f295b1b9f068fb22f9c15251dbeb09c
NyRakath <soncor23@gmail.com>
2021-11-01 13:23:14 -0500

    3 fix betty styles

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
 #ifndef ARG_GUARD
 #define ARG_GUARD
-#define ABS(x) (((x) < 0) ? (-(x)) : (x)) /* (-(x)) == ((x) * (-1)) law of signs... math logic */
+#define ABS(x) (((x) < 0) ? (-(x)) : (x))
+/* (-(x)) == ((x) * (-1)) law of signs... math logic */
 #endif
+
