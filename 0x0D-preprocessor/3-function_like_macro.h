26ea778ee2980e167278000ea5b65f3c9f6a2fea
NyRakath <soncor23@gmail.com>
2021-11-01 13:15:45 -0500

    3 - fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef HAVE_ABS
-#define HAVE_ABS
-#define ABS(x) ((x) < 0 ? (-(x)) : (x)) /* (-(x)) == ((x) * (-1)) law of signs... math logic */
+#ifndef HAVEABS
+#define HAVEABS
+#define ABS(x) (((x) < 0) ? (-(x)) : (x)) /* (-(x)) == ((x) * (-1)) law of signs... math logic */
 #endif
