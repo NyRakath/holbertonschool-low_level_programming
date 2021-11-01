49f57c6e26c800c63197677c4f88d976edd5a42b
NyRakath <soncor23@gmail.com>
2021-11-01 18:39:02 -0500

    0 header dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _STRUCT_DOG_
+#define _STRUCT_DOG_
+/**
+ * struct dog - A structure for a dates from a dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif /*_DOG_STRUCTURE_*/
