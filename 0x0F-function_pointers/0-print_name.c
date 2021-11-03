6a08cdf4de86db006255f658655744941cf18116
NyRakath <soncor23@gmail.com>
2021-11-03 14:08:41 -0500

    0 - print a name with a function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name with pointer function
+ * @name: into name of a person
+ * @f: is a pointer of a function callback again
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_P
+#define FUNCTION_P
+#include <unistd.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif /*FUNCTION_P*/
