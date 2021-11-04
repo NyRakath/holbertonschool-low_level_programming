28130b187286ccad95e8f090805e07e487fe48b6
NyRakath <soncor23@gmail.com>
2021-11-04 12:54:11 -0500

    0 - sum all arguments of a array

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all paramaters from main 
+ * @n: number of arguments
+ * Return: Sum of numbers from array with va_arg
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	sum = i = 0;
+	for (; i < (int)n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef HOLBER_H
+#define HOLBER_H
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+#endif /*END HOLBER_H*/
