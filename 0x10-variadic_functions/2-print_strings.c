b8591664254eab66145275d6c4e6ccf78125b5e2
NyRakath <soncor23@gmail.com>
2021-11-04 14:55:06 -0500

    2 - past logic  in the exercise

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", stri);
-		if (separator != NULL && i < (n - 1))
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
