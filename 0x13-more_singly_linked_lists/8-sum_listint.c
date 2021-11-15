8fe859cb504dc44e55e6c41cae5c3c0c10a8b7e8
NyRakath <Soncor23@gmail.com>
2021-11-15 16:09:10 -0500

    sum of all nodes of the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+* sum_listint - sum n of a listint_t list
+* @head: pointer to the head of the list
+* Return: sum all of all nodes or 0
+**/
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			sum += (*head).n;
+			head = (*head).next;
+		}
+	}
+	return (sum);
+}
