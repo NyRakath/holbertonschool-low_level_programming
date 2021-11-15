bbadccd5547d1ec277070e4e349de890ba7bca82
NyRakath <Soncor23@gmail.com>
2021-11-15 04:27:36 -0500

    Lists update prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,8 @@
 #define LISTS_H
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,4 +22,5 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
