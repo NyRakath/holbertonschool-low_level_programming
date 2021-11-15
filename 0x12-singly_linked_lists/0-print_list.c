ccf9354083eb394dd21330f2d3a1b33e1f074393
NyRakath <Soncor23@gmail.com>
2021-11-15 04:03:16 -0500

    Change for while

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,15 +6,16 @@
 **/
 size_t print_list(const list_t *h)
 {
-	size_t iter;
+	size_t iter = 0;
 
-	for (iter = 0; h; iter++)
+	while(h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		iter++;
 	}
 	return (iter);
 }
