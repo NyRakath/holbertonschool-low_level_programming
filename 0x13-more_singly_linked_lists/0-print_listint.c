f5d993c53b7b24b1a49d77e90a2961be86f62348
NyRakath <Soncor23@gmail.com>
2021-11-15 13:59:27 -0500

    print all elements of list integers

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* print_list - prints all nodes of a list
+* @h: pointer to elements of type list_t
+* Return: the number of nodes in the list
+**/
+size_t print_listint(const listint_t *h)
+{
+	size_t iter;
+
+	for (iter = 0; h; iter++)
+	{
+		printf("%d\n", h->n); /* (*h).n */
+		h = h->next; /* (*h).next */
+	}
+	return (iter);
+}
