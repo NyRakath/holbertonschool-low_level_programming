3d4bfae88365ac71fd268e3f7abc8cfade4307a0
NyRakath <Soncor23@gmail.com>
2021-11-23 19:06:30 -0500

    FIX betty styles

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		len = strlen(text_content);
 		file_wr = write(fd, text_content, len);
 		if (file_wr == -1)
-			return(-1);
+			return (-1);
 	}
 	close(fd);
 	return (1);
