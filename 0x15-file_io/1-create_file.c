c7bb31893253f43933bf18de4d299708181877fc
NyRakath <Soncor23@gmail.com>
2021-11-23 19:03:14 -0500

    create a new file and add text

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: name of file to be created
+ * @text_content: a NULL terminated string
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	mode_t mode;
+	ssize_t file_wr;
+
+	mode = S_IRUSR | S_IWUSR;
+	/* !filename */
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		len = strlen(text_content);
+		file_wr = write(fd, text_content, len);
+		if (file_wr == -1)
+			return(-1);
+	}
+	close(fd);
+	return (1);
+}
