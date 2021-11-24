15530d221d8004a2cebfb8efe3a09b4cacc2adfc
NyRakath <Soncor23@gmail.com>
2021-11-23 20:17:46 -0500

    Betty Styles

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
 * copy_file - copies the content of a file to another file
-* @file1: file to copy from
-* @fil2: file to copy to
+* @fileone: file to copy from
+* @filetwo: file to copy to
 * Return: nothing, or exit on failure
 **/
 void copy_file(const char *fileone, char *filetwo)
-{	
+{
 	ssize_t file_rd, file_wr;
 	int fd_1, fd_2, f_cls1, f_cls2;
 	char buf[BUFRSIZE];
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	
+
 	fd_1 = open(fileone, O_RDONLY);
 	if (fd_1 == -1)
 	{
@@ -24,7 +24,7 @@ void copy_file(const char *fileone, char *filetwo)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filetwo);
 		exit(99);
 	}
-	for (file_rd = 1 ;file_rd > 0;)
+	for (file_rd = 1 ; file_rd > 0;)
 	{
 		file_rd = read(fd_1, buf, BUFRSIZE);
 		if (file_rd == -1)
@@ -37,15 +37,14 @@ void copy_file(const char *fileone, char *filetwo)
 			file_wr = write(fd_2, buf, file_rd);
 			if (file_wr == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filetwo);
-				exit(99);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filetwo), exit(99);
 			}
 		}
 	}
 	f_cls1 = close(fd_1);
 	if (f_cls1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_cls1), exit(100);
-	f_cls2 = close(fd_2);	
+	f_cls2 = close(fd_2);
 	if (f_cls2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_cls2), exit(100);
 }
